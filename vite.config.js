00b5df251d92f7e0fea5c146a55a0aa4ed000f18
amiskiewiczpl <amiskiewiczpl@gmail.com>
2025-07-16 23:53:37 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/sportpage/', // <- ważne, jeśli wrzucisz na GitHub Pages
+  base: './', // działa też lokalnie
 })
