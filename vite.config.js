2dd19517743affb6e37a7aa0c2d5fc9d35099d96
amiskiewiczpl <amiskiewiczpl@gmail.com>
2025-07-17 23:13:53 +0200

    git

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
+//import { defineConfig } from 'vite'
+// react from '@vitejs/plugin-react'
+//
+//export default defineConfig({
+//  plugins: [react()],
+//  base: './', // działa też lokalnie
+//})
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  base: './', // działa też lokalnie
+  // ścieżka, pod którą będzie serwowany build na GH Pages
+  base: '/sportpage/',
+
+  plugins: [
+    react()
+  ],
 })
