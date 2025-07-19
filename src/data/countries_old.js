3a1d8be72c081a3d22ecfab796acab935765e1d4
amiskiewiczpl <amiskiewiczpl@gmail.com>
2025-07-19 23:30:41 +0200

    j

---
+++ src/data/countries_old.js
@@ -0,0 +1,82 @@
+// src/data/countries.js
+
+const countries = {
+  poland: {
+    aliases: ['pol', 'polska', 'poland'],
+    flagUrl: 'https://flagcdn.com/pl.svg',
+    name: 'Polska',
+    medals: {
+      gold: 75,
+      silver: 91,
+      bronze: 140,
+    },
+    summerGames: [
+      { year: 2000, gold: 6, silver: 5, bronze: 3 },
+      { year: 2004, gold: 3, silver: 2, bronze: 5 },
+      { year: 2008, gold: 3, silver: 6, bronze: 1 },
+      { year: 2012, gold: 2, silver: 2, bronze: 6 },
+      { year: 2016, gold: 2, silver: 3, bronze: 6 },
+      { year: 2020, gold: 4, silver: 5, bronze: 5 },
+    ],
+  },
+
+  germany: {
+    aliases: ['ger', 'niemcy', 'germany'],
+    flagUrl: 'https://flagcdn.com/de.svg',
+    name: 'Niemcy',
+    medals: {
+      gold: 293,
+      silver: 293,
+      bronze: 306,
+    },
+    summerGames: [
+      { year: 2000, gold: 13, silver: 17, bronze: 26 },
+      { year: 2004, gold: 13, silver: 16, bronze: 20 },
+      { year: 2008, gold: 16, silver: 10, bronze: 15 },
+      { year: 2012, gold: 11, silver: 19, bronze: 14 },
+      { year: 2016, gold: 17, silver: 10, bronze: 15 },
+      { year: 2020, gold: 10, silver: 11, bronze: 16 },
+    ],
+  },
+usa: {
+  aliases: ['usa', 'stany', 'united states'],
+  flagUrl: 'https://flagcdn.com/us.svg',
+  name: 'USA',
+  medals: {
+    gold: 1063,
+    silver: 836,
+    bronze: 739,
+  },
+  summerGames: [
+    { year: 2000, gold: 37, silver: 24, bronze: 32 },
+    { year: 2004, gold: 36, silver: 39, bronze: 26 },
+    { year: 2008, gold: 36, silver: 38, bronze: 36 },
+    { year: 2012, gold: 46, silver: 28, bronze: 29 },
+    { year: 2016, gold: 46, silver: 37, bronze: 38 },
+    { year: 2020, gold: 39, silver: 41, bronze: 33 },
+  ],
+},
+
+  france: {
+    aliases: ['fra', 'france', 'francja'],
+    flagUrl: 'https://flagcdn.com/fr.svg',
+    name: 'Francja',
+    medals: {
+      gold: 264,
+      silver: 293,
+      bronze: 327,
+    },
+    summerGames: [
+      { year: 2000, gold: 13, silver: 14, bronze: 11 },
+      { year: 2004, gold: 11, silver: 9, bronze: 13 },
+      { year: 2008, gold: 7, silver: 16, bronze: 17 },
+      { year: 2012, gold: 11, silver: 11, bronze: 12 },
+      { year: 2016, gold: 10, silver: 18, bronze: 14 },
+      { year: 2020, gold: 10, silver: 12, bronze: 11 },
+    ],
+  },
+
+  // Dodasz więcej, jak chcesz: italy, usa, china itd.
+}
+
+export default countries;
