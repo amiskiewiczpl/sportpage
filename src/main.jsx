e6ce8acdbf99ad7ddf28153581d1a07bffff28d3
amiskiewiczpl <amiskiewiczpl@gmail.com>
2025-07-19 03:09:41 +0200

    a

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { HashRouter } from 'react-router-dom';
+
+<HashRouter>
+  <App />
+</HashRouter>
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
